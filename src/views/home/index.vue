5fe40f6039be848ed5b3ecf35988f94f9a7a4b47
Sabir Ameen <sabir.ameen93@gmail.com>
2025-03-20 20:36:11 +0530

    app initial push

---
+++ src/views/home/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const onNewMeeting = () => {
+  router.push('/meeting/asdasdasd')
+}
+</script>
+
+<template>
+  <main class="w-full">
+    <v-sheet
+      border="md"
+      class="pa-6 w-[80vw] max-w-none min-h-[400px] text-white mx-auto !flex gap-[32px] justify-center items-center flex-col"
+      color="#141518"
+    >
+      <v-btn size="x-large" density="default" color="indigo-darken-3" @click="onNewMeeting">
+        New Meeting
+      </v-btn>
+      <v-btn size="x-large" density="default" color="blue">Join Meeting</v-btn>
+    </v-sheet>
+  </main>
+</template>
+
+<style>
+@import 'tailwindcss';
+</style>
---
+++ src/views/meeting/index.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import Peer from 'peerjs'
+import { onMounted, ref, useTemplateRef } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+const callVideoRef = useTemplateRef('call-ref')
+const receiveVideoRef = useTemplateRef('receive-ref')
+const route = useRoute()
+const router = useRouter()
+const currMediaStream = ref()
+
+onMounted(() => {
+  const peerKey = sessionStorage.getItem('peerKey') ?? ''
+  const peer = new Peer(peerKey, {
+    host: 'face2face-peer-server.onrender.com',
+    secure: true,
+  })
+  peer.on('open', function (id) {
+    console.log('My peer ID is: ' + id)
+    if (!route.params.id) {
+      router.replace(`/meeting/${id}`)
+    }
+    sessionStorage.setItem('peerKey', id)
+  })
+  peer.on('call', function (call) {
+    // Answer the call, providing our mediaStream
+    console.log('call')
+    call.answer(currMediaStream.value)
+    call.on('stream', function (stream) {
+      // `stream` is the MediaStream of the remote peer.
+      // Here you'd add it to an HTML video/canvas element.
+      if (receiveVideoRef?.value) receiveVideoRef.value.srcObject = stream
+    })
+  })
+  navigator.mediaDevices
+    .getUserMedia({
+      video: true,
+      audio: true,
+    })
+    .then((stream) => {
+      if (callVideoRef?.value) callVideoRef.value.srcObject = stream
+      currMediaStream.value = stream
+      if (route.params.id !== sessionStorage.getItem('peerKey')) {
+        peer.call(route.params.id as string, stream)
+      }
+    })
+})
+</script>
+
+<template>
+  <main class="w-screen">
+    <section class="w-screen h-screen relative">
+      <video class="w-full h-full" autoplay ref="call-ref" muted></video>
+      <section class="absolute bottom-[20px] left-0 right-0 flex justify-center">
+        <section>asdasd</section>
+        <video
+          class="absolute right-0 bottom-0 w-[400px] h-[400px]"
+          autoplay
+          ref="receive-ref"
+          muted
+        ></video>
+      </section>
+    </section>
+  </main>
+</template>
+
+<style>
+@import 'tailwindcss';
+</style>
